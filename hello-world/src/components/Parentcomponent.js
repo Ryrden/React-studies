1d7236612be21427e1415900d1dfdff4d562e9bd
Ryan S <ryansateles@gmail.com>
2022-08-10 15:18:55 -0300

    lesson 15 - method as props

---
+++ hello-world/src/components/Parentcomponent.js
@@ -0,0 +1,28 @@
+import React, {Component} from "react";
+import ChildComponent from "./ChildComponent";
+
+class Parentcomponent extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			parentName: "Parent",
+		};
+
+		this.greetparent = this.greetparent.bind(this);
+	}
+
+	greetparent(childName) {
+		alert(`Hello ${this.state.parentName} from ${childName}`);
+	}
+
+	render() {
+		return (
+			<div>
+				<ChildComponent greetHandler={this.greetparent} />
+			</div>
+		);
+	}
+}
+
+export default Parentcomponent;
