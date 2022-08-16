c958e852451b2f706b1466082b2d8c33ad9f7954
Ryan S <ryansateles@gmail.com>
2022-08-16 16:05:51 -0300

    lesson 8 - Conditionally run effects

--- react-hooks/src/components/ClassCounterOne.js
+++ react-hooks/src/components/ClassCounterOne.js
@@ -6,21 +6,32 @@ class ClassCounterOne extends Component {
 
 		this.state = {
 			count: 0,
+			name: "",
 		};
 	}
 
 	componentDidMount() {
 		document.title = `Clicked  ${this.state.count} times`;
 	}
-    
-    componentDidUpdate(prevProps, prevState) {
-        document.title = `Clicked  ${this.state.count} times`;
-        
-    }
+
+	//How to only update the title when the count changes
+	componentDidUpdate(prevProps, prevState) {
+		if (prevState !== this.state.count) {
+			console.log("Updating document title");
+			document.title = `Clicked  ${this.state.count} times`;
+		}
+	}
 
 	render() {
 		return (
 			<div>
+				<input
+					type="text"
+					value={this.state.name}
+					onChange={(e) => {
+						this.setState({name: e.target.value});
+					}}
+				/>
 				<button onClick={() => this.setState({count: this.state.count + 1})}>
 					Click {this.state.count} times
 				</button>
--- react-hooks/src/components/HookCounterOne.js
+++ react-hooks/src/components/HookCounterOne.js
@@ -2,17 +2,21 @@ import React, {useState, useEffect} from "react";
 
 function HookCounterOne() {
 	const [count, setCount] = useState(0);
+	const [name, setName] = useState("");
 
 	/* UseEffect
 		1. The useEffect hook runs after every render
 	*/
 
+	//The second parameter is an array of dependencies to observe.
 	useEffect(() => {
+		console.log("useEffect - Updating document title");
 		document.title = `You clicked ${count} times`;
-	});
+	}, [count]);
 
 	return (
 		<div>
+			<input type="text" value={name} onChange={(e) => setName(e.target.value)} />
 			<button onClick={() => setCount(count + 1)}>Click {count} times</button>
 		</div>
 	);
