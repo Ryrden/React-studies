52a0e20031d1a2951d313e99db30234c85d1b4f2
Ryan S <ryansateles@gmail.com>
2022-09-01 15:35:16 -0300

    lesson 1 - Configuring Routes

---
+++ react-router/src/App.js
@@ -0,0 +1,15 @@
+import {Routes, Route} from "react-router-dom";
+import "./App.css";
+import {Home} from "./components/Home";
+import {About} from "./components/About";
+
+function App() {
+	return (
+		<Routes>
+			<Route path="/" element={<Home />}></Route>
+			<Route path="about" element={<About />}></Route>
+		</Routes>
+	);
+}
+
+export default App;
