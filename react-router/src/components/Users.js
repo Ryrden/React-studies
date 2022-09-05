54ec362171b58cb38a59cb73946f8f2bc1259866
Ryan S <ryansateles@gmail.com>
2022-09-05 13:51:37 -0300

    lesson 10 - Search Params (filtering)

--- react-router/src/components/Users.js
+++ react-router/src/components/Users.js
@@ -1,12 +1,23 @@
-import {Outlet} from "react-router-dom";
+import {Outlet, useSearchParams} from "react-router-dom";
+
+/* 
+searchParams is usually when applying filters to a list of items
+*/
 
 export const Users = () => {
+	const [searchParams, setSearchParams] = useSearchParams();
+	const showActiveUsers = searchParams.get("filter") === "active";
 	return (
 		<div>
 			<h2>User 1</h2>
 			<h2>User 2</h2>
 			<h2>User 3</h2>
 			<Outlet />
+			<div>
+				<button onClick={() => setSearchParams({filter: "active"})}>Active Users</button>
+				<button onClick={() => setSearchParams({})}>Reset Filter</button>
+			</div>
+			{showActiveUsers ? <h2>Active Users</h2> : <h2>All Users</h2>}
 		</div>
 	);
 };
