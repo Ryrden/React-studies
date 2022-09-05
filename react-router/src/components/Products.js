ebbeabe34f1c790cc3a037338027b22e54b84e43
Ryan S <ryansateles@gmail.com>
2022-09-05 13:58:48 -0300

    lesson 11 - Relative Links and Absolute Links

--- react-router/src/components/Products.js
+++ react-router/src/components/Products.js
@@ -8,8 +8,13 @@ export const Products = () => {
 				<input type="search" placeholder="Search product" />
 			</div>
 			<nav>
+				{/* Relative Links: */}
 				<Link to="featured">Featured</Link>
 				<Link to="new">New</Link>
+				{/* Absolute Links
+				<Link to="/products/featured">Featured</Link>
+				<Link to="/products/new">New</Link> 
+				*/}
 			</nav>
 			<Outlet /> {/* This is where the child routes will be rendered */}
 		</>
